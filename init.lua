30da9dc8d2a262c64c8604f0bdc66969152f987c
Kemal Ardıl Gülez <ardilgulez@gmail.com>
2025-04-01 09:16:13 +0200

    Nvim Tmux Nav

--- init.lua
+++ init.lua
@@ -303,6 +303,7 @@ require('lazy').setup({
   require 'custom.plugins.comment',
   -- Color on autocomplete and tailwind classes
   require 'custom.plugins.colorizer',
+  require 'custom.plugins.nvim-tmux-navigator',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
--- lua/kickstart/plugins/conform-nvim.lua
+++ lua/kickstart/plugins/conform-nvim.lua
@@ -34,7 +34,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'black', 'ruff', stop_after_first = true },
+        python = { 'ruff', stop_after_first = true },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         html = { 'prettierd', 'prettier', stop_after_first = true },
